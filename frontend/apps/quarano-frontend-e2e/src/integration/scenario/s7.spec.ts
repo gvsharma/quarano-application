ab7b08264bae40b72b424273f5ceaf87ec1c9d51
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2021-01-10 12:57:10 +0100

    CORE-676 e2e/s7 implemented until step 30, renamed some data-cy

--- frontend/apps/quarano-frontend-e2e/src/integration/scenario/s7.spec.ts
+++ frontend/apps/quarano-frontend-e2e/src/integration/scenario/s7.spec.ts
@@ -20,7 +20,8 @@
 --> CHECK: Tab "Email-Vorlage" ist vorhanden
 14A - Aufrufen der E-Mail Vorlage
 --> CHECK: Button "Aktivierungscode erneuern" ist vorhanden
-15 - Extrahiere Anmeldelink aus dem Template16 - Logout als GAMA
+15 - Extrahiere Anmeldelink aus dem Template
+16 - Logout als GAMA
 17 - Anmeldelink aufrufen
 18 - Klick auf Weiter
 19 - Benutzername: "Berta"
@@ -65,6 +66,24 @@ describe('S7 - Status wechselt korrekt', () => {
   before((done) => {
     cy.restartBackend(done);
   });
+
+  function extractActivationCode(elem: JQuery) {
+    const regex = /\/client\/enrollment\/landing\/index\/(.*)/g;
+    let content;
+
+    if (typeof elem !== 'string') {
+      content = elem.text();
+    } else {
+      content = elem;
+    }
+
+    try {
+      return regex.exec(content)[1];
+    } catch (e) {
+      cy.log(e);
+      throw e;
+    }
+  }
 
   it('should run', () => {
     /* 0 - Login als Gama "agent1" */
@@ -136,42 +155,99 @@ describe('S7 - Status wechselt korrekt', () => {
     cy.get('[data-cy="start-tracking-button"]').click();
 
     /* CHECK: In Übersicht "Indexfälle" steht für "Berta Benz" der Status "in Registrierung" */
+    cy.get('[data-cy="case-status"]').contains('in Registrierung');
 
     /* CHECK: Tab "Email-Vorlage" ist vorhanden */
     cy.get('.mat-tab-links').children().should('have.length', 6);
     cy.get('.mat-tab-links').children().contains('E-Mail Vorlage');
 
     /* 14A - Aufrufen der E-Mail Vorlage */
-    cy.get('[data-cy="tab-link-email"]').should('exist').click();
+    cy.get('[data-cy="email-tab"]').should('exist').click();
     cy.wait(500);
 
     /* CHECK: Button "Aktivierungscode erneuern" ist vorhanden */
-    cy.get('[data-cy="button-renew-activation-code"]').should('exist');
+    cy.get('[data-cy="new-activation-code"]').should('exist');
 
-    cy.get('qro-client-mail > div > pre')
+    cy.get('[data-cy="mail-text"]')
       .should('exist')
       .then((elem) => {
         /* 15 - Extrahiere Anmeldelink aus dem Template */
-        const regex = /https:\/\/.*\/client\/enrollment\/landing\/index\/.*/gm;
-        let content;
-        let url = '';
-
-        if (typeof elem !== 'string') {
-          content = elem.text();
-        } else {
-          content = elem;
-        }
-
-        const urls = regex.exec(content);
-        if (urls && urls.length !== 0) {
-          url = urls[0];
-        }
-
-        expect(url).to.contain('client/enrollment/landing/index');
+        const extractedActivationCode = extractActivationCode(elem);
 
         /* 16 - Logout als GAMA */
-        cy.get('[data-cy="profile-user-button"]').should('exist').click();
-        cy.get('[data-cy="logout-button"]').should('exist').click();
+        cy.logOut();
+
+        /* CHECK: Logout war erfolgreich */
+        cy.get('[data-cy="profile-user-button"]').should('not.exist');
+
+        /* 17 - Anmeldelink aufrufen */
+        cy.visit('/client/enrollment/landing/index/' + extractedActivationCode);
+
+        /* 18 - Klick auf Weiter */
+        cy.get('[data-cy="cta-button-index"]').click();
+        cy.get('[data-cy="registration-submit-button"] button').should('be.disabled');
+
+        /* 19 - Benutzername: "Berta" */
+        cy.get('[data-cy="input-username"] input[matInput]').type('Berta');
+
+        /* 20 - Passwort: "Password03!" */
+        cy.get('[data-cy="input-password"] input[matInput]').type('Password03!');
+
+        /* 21 - Passwort bestätgen  "Password03!" */
+        cy.get('[data-cy="input-password-confirm"] input[matInput]').type('Password03!');
+
+        /* 22 - Geburtsdatum: "25.03.1946" */
+        cy.get('[data-cy="input-dateofbirth"] input[matInput]').type('25.03.1946');
+
+        /* 23 - AGB aktivieren */
+        cy.get('[data-cy="input-privacy-policy"]').click();
+
+        /* 24 - Klick auf "Registrieren" Button */
+        cy.get('[data-cy="registration-submit-button"] button').should('be.enabled');
+        cy.get('[data-cy="registration-submit-button"] button').click();
+
+        /* 25 - Klick auf "weiter" */
+        cy.get('[data-cy="first-step-button"] button').click();
+
+        /* 26 - Logout als Bürger */
+        cy.logOut();
+
+        /* 27 -  Login als Gama "agent1" */
+        cy.logInAgent();
+
+        /* CHECK:  In Übersicht "Indexfälle" steht für "Berta Benz" der Status "Registrierung abgeschlossen" */
+        cy.get('[data-cy="search-case-input"]').should('exist').type('Berta Benz');
+        cy.get('.ag-center-cols-container > div > [col-id="status"]').contains('Registrierung abgeschlossen');
+
+        /* 28 - Logout als GAMA */
+        cy.logOut();
+
+        /* 29 - Login als Bürger ("Berta"; "Password03!") */
+        cy.logIn('Berta', 'Password03!');
+        /* 30 - Initialer Fragebogen "Covid-19-Symptome" -> "nein" */
+        /* 31 - Bitte geben Sie Ihren behandelnden Hausarzt an. -> Dr. Schmidt */
+        /* 32 - Nennen Sie uns bitte den (vermuteten) Ort der Ansteckung: -> "Familie" */
+        /* 33 - Haben Sie eine oder mehrere relevante Vorerkrankungen? -> "nein" */
+        /* 34 - Arbeiten Sie im medizinischen Umfeld oder in der Pflege? -> "nein" */
+        /* 35 - Haben Sie Kontakt zu Risikopersonen? -> "nein" */
+        /* 36 - Klick "weiter" */
+        /* 37 - Kontakte mit anderen Menschen -> "Carl Benz" */
+        /* 38 - Klick enter */
+        /* 39 - wähle "Kontakt anlegen" in Popup */
+        /* 40 - Telefonnummer (mobil) -> "017196347526" */
+        /* 41 - Klick auf "speichern" */
+        /* 42 - Klick auf "Erfassung abschließen" */
+        /* 43 - Logout als Bürger */
+        /* 44 - Login als GAMA "agent1" */
+        /* --> CHECK: In Übersicht "Indexfälle" steht für "Berta Benz" der Status "In Nachverfolgung" */
+        /* 45 - suche Indexfall "Berta Benz" */
+        /* 46 - wähle "Fall abschließen" */
+        /* 47 - Popup "Diesen Fall abschließen" geht auf */
+        /* 48 - Zusätzliche Informationen zum Fallabschluss: -> "Quarantäne beendet" */
+        /* 49 - Klicke "OK" */
+        /* 50 - wähle in Übersicht der Indexfälle den Filter "abgeschlossen" */
+        /* --> CHECK: In Übersicht "Indexfälle" steht für "Berta Benz" der Status "abgeschlossen" */
+        /* 51 - Logout als GAMA */
       });
   });
 });
